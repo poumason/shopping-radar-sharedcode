35311a28fd56ac0e22fd135bdc7e63e5ee83bf2e
Pou Lin <poumason@live.com>
2021-06-17 00:06:17 +0800

    fixed chat api bug

--- src/db/chat_api.js
+++ src/db/chat_api.js
@@ -4,7 +4,7 @@ class ChatAPI extends AirtableAPI {
   async getChats () {
     const response = await this.instance.get('/Chats', {
       params: {
-        filterByFormula: '{bot_type}=telegram'
+        filterByFormula: '{bot_type}="telegram"'
       }
     });
     return response.data;
