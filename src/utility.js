0ac6d931444e2bc68e0ac4046c9f87fc3c039d7a
pou lin <poumason@live.com>
2022-03-31 17:47:23 +0800

    fixed for each bug

--- src/utility.js
+++ src/utility.js
@@ -6,7 +6,7 @@ function isAvailablePrice (price, ignoreTags) {
   // 金額異常高為額滿商品請勿下標 ( 例:尾數XXX9999 或 XXX0000)
   const strPrice = price.toString();
 
-  for (const tag in ignoreTags) {
+  for (const tag of ignoreTags) {
     const index = strPrice.indexOf(tag);
 
     if (index < 0) {
